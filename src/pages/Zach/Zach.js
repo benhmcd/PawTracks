97c6d792eace16c1bdfddcd837f4e43406fc4afe
Ben McDonald <benhmcd@gmail.com>
2023-03-09 18:02:02 -0500

    fixed image

--- src/pages/Zach/Zach.js
+++ src/pages/Zach/Zach.js
@@ -6,9 +6,9 @@ import './Zach.css';
 function Zach() {
 return (
     <>
-    <img id='nineEleven' src='/jokeImages/WorldTradeCenters.webp'></img>
+    <img id='nineEleven' src='./jokeImages/WorldTradeCenters.webp'></img>
     <h1 id='threat'>الموت لزاك</h1>
-    <video id='hostage' loop controls src='/jokeImages/HostageVideo.mp4'></video>
+    <video id='hostage' loop controls src='./jokeImages/HostageVideo.mp4'></video>
     <h1 id='message'> If you EVER want to see Zach again, you must play poker tonight and watch the NC State Basketball game.</h1>
     </>
 )
