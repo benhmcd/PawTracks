43a472bb350d7b3acd1ad70cca3fc8d08d7206ab
g445g445 <bebpower@gmail.com>
2023-03-30 13:47:43 -0400

    (SP2-004) - Testing in deployment now
    Added cypress, testing it in ENV

---
+++ cypress/integration/PawTracks/test.spec.js
@@ -0,0 +1,17 @@
+describe('test_name', function () {
+
+    it('what_it_does', function () {
+
+        cy.viewport(1920, 929)
+        cy.visit('http://localhost:3000/');
+        
+        cy.get('#amplify-id-0').type('g445');
+        cy.get('#amplify-id-2').type('Testuser1!');
+        cy.get('.amplify-button--primary').click();
+        cy.get('form').submit();
+        cy.get('.signOut').click();
+
+    })
+
+})
+
