5b8df51f2ba2eab8b5f75915a9d475c7caad6980
g445g445 <bebpower@gmail.com>
2023-04-20 14:57:08 -0400

    BUG FIX: Pets Infinite Loading Screen
    Fix a bug where if the user has no pets, the page will wait on data to return for ever.

--- src/pages/Pets/Pets.js
+++ src/pages/Pets/Pets.js
@@ -1,5 +1,5 @@
 // Import required dependencies
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import ReactModal from 'react-modal';
 import AddPet from './AddPet';
@@ -29,7 +29,10 @@ function Pets() {
   const [imageURLs, setImageURLs] = useState({});
   const [currentPet, setCurrentPet] = useState([]);
   const [isLoading, setIsLoading] = useState(true); // add a loading state
-
+  //const [isInitialLoad, setIsInitialLoad] = useState(true); // add a flag for initial load
+  //const [numLoops, setNumLoops] = useState(0);
+  const LOOPS_BEFORE_LOAD = 3;
+  var numLoopsRef = useRef(0);
   let subscription; // declare subscription outside of the useEffect callback
 
   useEffect(() => {
@@ -41,10 +44,12 @@ function Pets() {
       const subscription = DataStore.observeQuery(PetModel).subscribe(({ items }) => {
         setPet(items)
         console.log(items)
+
       });
     };
     // call the function to fetch the data
     getDate();
+
     return () => {
       if (subscription) {
         subscription.unsubscribe();
@@ -67,8 +72,21 @@ function Pets() {
         console.error('Error fetching image URL:', error);
       }
     };
+    // Check how many loops before loading and whether there are pets available
+    console.log(`Loops Before Loading: ${numLoopsRef.current}... Is pets available: ${pets.length}`);
+
+    // Increment the loop counter
+    numLoopsRef.current += 1;
+
+    // If there are no pets and the loop threshold has been reached, stop loading
+    if (numLoopsRef.current >= LOOPS_BEFORE_LOAD && pets.length === 0) {
+      setIsLoading(false);
+    }
+
+    // Fetch the image URLs for each pet
     pets.forEach((item) => getImageURL(item.id));
   }, [pets]);
+
 
   const handleDelete = async (petDelete) => {
     try {
@@ -103,75 +121,75 @@ function Pets() {
 
   return (
     <>
-      
-        <h1>Pets</h1>
-        <br />
-        <div className='addPet-container'>
-          {/* <Link to='/pets/addPet'><button className="add-button">Add Pets</button></Link> */}
-          <button className="add-button" onClick={() => {togglePetAdd()}}>Add Pets</button>
-        </div>
-        <ReactModal
-          isOpen={addPetIsOpen}
-          onRequestClose={togglePetAdd}
-          contentLabel='Add a pet'
-          className='pet-modal'
-          overlayClassName='pet-overlay'
-          closeTimeoutMS={500}
-        >
-          <AddPet onFormClose={onAddPetFormClose} />
-        </ReactModal>
-        <ReactModal
-          isOpen={editPetIsOpen}
-          onRequestClose={togglePetEdit}
-          contentLabel='Edit a pet'
-          className='pet-modal'
-          overlayClassName='pet-overlay'
-          closeTimeoutMS={500}
-        >
-          <EditPet onFormClose={onEditPetFormClose} petId={currentPet} />
-        </ReactModal>
-        <div className="cards">
-          {pets.map((item) => (
-            <Card className="pet-card">
-              <div className='pet-content'>
-                <div className="pet-head">
-                  <header className='pet-name'>
-                    {item.name}
-                  </header>
+
+      <h1>Pets</h1>
+      <br />
+      <div className='addPet-container'>
+        {/* <Link to='/pets/addPet'><button className="add-button">Add Pets</button></Link> */}
+        <button className="add-button" onClick={() => { togglePetAdd() }}>Add Pets</button>
+      </div>
+      <ReactModal
+        isOpen={addPetIsOpen}
+        onRequestClose={togglePetAdd}
+        contentLabel='Add a pet'
+        className='pet-modal'
+        overlayClassName='pet-overlay'
+        closeTimeoutMS={500}
+      >
+        <AddPet onFormClose={onAddPetFormClose} />
+      </ReactModal>
+      <ReactModal
+        isOpen={editPetIsOpen}
+        onRequestClose={togglePetEdit}
+        contentLabel='Edit a pet'
+        className='pet-modal'
+        overlayClassName='pet-overlay'
+        closeTimeoutMS={500}
+      >
+        <EditPet onFormClose={onEditPetFormClose} petId={currentPet} />
+      </ReactModal>
+      <div className="cards">
+        {pets.map((item) => (
+          <Card className="pet-card">
+            <div className='pet-content'>
+              <div className="pet-head">
+                <header className='pet-name'>
+                  {item.name}
+                </header>
+              </div>
+              <Divider />
+              {imageURLs[item.id] && (
+                <div className="pet-image-container">
+                  <img src={imageURLs[item.id]} alt={item.name} className="pet-image" />
                 </div>
-                <Divider />
-                {imageURLs[item.id] && (
-                  <div className="pet-image-container">
-                    <img src={imageURLs[item.id]} alt={item.name} className="pet-image" />
-                  </div>
-                )}
-                <div className='pet-info'>
-                  <h5>{item.image}</h5>
-                  <h5>Type:</h5> <p>{item.type[0].toUpperCase() + item.type.slice(1).toLowerCase()}</p>
-                  <h5>Weight:</h5> <p>{item.weight}</p>
-                  <h5>Age:</h5> <p>{item.age}</p>
-                </div>
-                <div className="pet-buttons">
-                  {/* <Link to={`/pets/${items.id}`} key={items.id} className="edit-link">
+              )}
+              <div className='pet-info'>
+                <h5>{item.image}</h5>
+                <h5>Type:</h5> <p>{item.type[0].toUpperCase() + item.type.slice(1).toLowerCase()}</p>
+                <h5>Weight:</h5> <p>{item.weight}</p>
+                <h5>Age:</h5> <p>{item.age}</p>
+              </div>
+              <div className="pet-buttons">
+                {/* <Link to={`/pets/${items.id}`} key={items.id} className="edit-link">
                     <IconContext.Provider value={{ className: "edit-icon" }}>
                       <MdOutlineEdit />
                     </IconContext.Provider>
                   </Link> */}
-                  <button className='edit-button' onClick={() => togglePetEdit(item.id)}>
-                    <IconContext.Provider value={{ className: "edit-icon" }}>
-                      <MdOutlineEdit />
-                    </IconContext.Provider>
-                  </button>
-                  <button className='delete-button' onClick={() => handleDelete(item)}>
-                    <IconContext.Provider value={{ className: "delete-icon" }}>
-                      <RiDeleteBin5Line />
-                    </IconContext.Provider>
-                  </button>
-                </div>
+                <button className='edit-button' onClick={() => togglePetEdit(item.id)}>
+                  <IconContext.Provider value={{ className: "edit-icon" }}>
+                    <MdOutlineEdit />
+                  </IconContext.Provider>
+                </button>
+                <button className='delete-button' onClick={() => handleDelete(item)}>
+                  <IconContext.Provider value={{ className: "delete-icon" }}>
+                    <RiDeleteBin5Line />
+                  </IconContext.Provider>
+                </button>
               </div>
-            </Card>
-          ))}
-        </div>
+            </div>
+          </Card>
+        ))}
+      </div>
     </>
   )
 }
