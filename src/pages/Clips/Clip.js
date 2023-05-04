b6237db84f876f3f4d01078a8df2adf2af8de7a7
g445g445 <bebpower@gmail.com>
2023-05-04 11:50:51 -0400

    added video to single clip page

--- src/pages/Clips/Clip.js
+++ src/pages/Clips/Clip.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import './Clip.css';
+import { Storage } from 'aws-amplify';
 
 function Clip() {
-    const {id} = useParams();
+    const { id } = useParams();
+    const [videoUrl, setVideoUrl] = useState(null);
+
+    useEffect(() => {
+        async function fetchVideoUrl() {
+            const url = await Storage.get(id, { level: 'private' });
+            setVideoUrl(url);
+        }
+        fetchVideoUrl();
+    }, [id]);
+
+    if (!videoUrl) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <>
             <h1>Single Clip Page: {id}</h1>
+            <div>
+                <video controls src={videoUrl}></video>
+            </div>
         </>
     )
 }
